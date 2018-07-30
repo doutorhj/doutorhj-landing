636f4edc71c6ad819c1e0994569a741bcd3b9d3c
Paulo Kenobi <kenobipaulo@gmail.com>
2018-07-30 17:06:59 -0300

    Checkup Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -500,7 +500,82 @@
             </div>
             <div id="checkup" class="area-checkup">
                 <div class="container">
-                    Checkup
+                    <div class="selo">
+                        <img src="/libs/home-template/img/ckp-selo.png" alt="">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="pacote-checkup">
+                                <h3>CheckUp Masculino</h3>
+                                <p class="qtde-procedimentos">9 Procedimentos</p>
+                                <ul>
+                                    <li>
+                                        <p><strong>1 Consulta:</strong> Consulta urológica</p>
+                                    </li>
+                                    <li>
+                                        <p><strong>7 Exames:</strong> Creatina; Lipidograma; Glicemia de Jejum; Hemograma Completo; TGO; TGP; Urina I</p>
+                                    </li>
+                                    <li>
+                                        <p><strong>1 Imagem:</strong> Ultrassom da prostata</p>
+                                    </li>
+                                </ul>
+                                <div class="valor">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <p class="vlr-mercado"><strike>R$ 610,00</strike></p>
+                                            <p>valor de mercado</p>
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p class="txt-auxiliar">por</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="vlr-drhj">R$ 350,00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="area-btn">
+                                    <button type="button" class="btn btn-agendar-plano-1">Agendar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="pacote-checkup">
+                                <h3>CheckUp Feminino</h3>
+                                <p class="qtde-procedimentos">12 Procedimentos</p>
+                                <ul>
+                                    <li>
+                                        <p><strong>1 Consulta:</strong> Consulta ginecológica</p>
+                                    </li>
+                                    <li>
+                                        <p><strong>8 Exames:</strong> Creatina; Lipidograma; Glicemia de Jejum; Hemograma Completo; TGO; TGP; TSH; Urina I</p>
+                                    </li>
+                                    <li>
+                                        <p><strong>2 Imagens:</strong> Ultrassom das mamas; Ultrasom transvaginal</p>
+                                    </li>
+                                    <li>
+                                        <p><strong>Exame clínico laboratorial:</strong> Papanicolau</p>
+                                    </li>
+                                </ul>
+                                <div class="valor">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <p class="vlr-mercado"><strike>R$ 638,00</strike></p>
+                                            <p>valor de mercado</p>
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p class="txt-auxiliar">por</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="vlr-drhj">R$ 438,00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="area-btn">
+                                    <button type="button" class="btn btn-agendar-plano-2">Agendar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div id="vantagens" class="area-vantagens">
